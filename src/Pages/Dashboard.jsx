b397184d9826458f720c65f0359ddd7823a2736c
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-01-07 11:21:46 +0100

    created dashboard

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from '../Components/Sidebar'
+
+const Dashboard = () => {
+  return (
+    <body className='dashboard-main'>
+    <header>
+      <nav>
+        <Sidebar />
+      </nav>
+    </header>
+    </body>
+  )
+}
+
+export default Dashboard
