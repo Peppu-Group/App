e605bd2145abbfadc2148f28e5d324f810d71c9c
Bishop ukpai <bishopukpai0@gmail.com>
2023-01-09 15:41:21 +0100

    added the icons

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import IMG_2437 from '../assets/IMG_2437.png'
+import {RiDashboardFill} from 'react-icons/ri'
+import {IoMdNotificationsOutline} from 'react-icons/io'
+import {MdOutlineHelpCenter} from 'react-icons/md'
+import {FaFileInvoiceDollar} from 'react-icons/fa'
+import {GrUserSettings} from 'react-icons/gr'
 const Sidebar = () => {
     function readCookie(name) {
         var nameEQ = name + "=";
@@ -28,12 +33,37 @@ const Sidebar = () => {
                  <section className='user-info'>
                     <a href='/'><img className='user-image' src={url}/></a>
                     <a href='/'><li className='username'>{JSON.parse(readCookie('userinfo')).username}</li></a>
-                 </section>    
+                 </section>  
+                 <section className='icons'>
+                    <section className='dash-icon'>
+                       <RiDashboardFill />
+                    </section>
                     <a href='/'><li>Dashboard</li></a>
+                 </section>  
+                 <section className='icons'>
+                    <section className='invoice-icon'>
+                        <FaFileInvoiceDollar/>
+                    </section>
                     <a href='/invoices'><li>Invoices</li></a>
-                    <a href='/notifications'><li>Notifications</li></a>
+                 </section>
+                 <section className='icons'>
+                    <section className='icon'>
+                        <IoMdNotificationsOutline/>
+                    </section>
+                    <a href='/notifications'><li className='notification'>Notifications</li></a>
+                 </section>
+                 <section className='icons'>
+                    <section className='help-icon'>
+                       <MdOutlineHelpCenter />
+                    </section>
                     <a href='/help'><li>Help</li></a>
+                 </section>
+                 <section className='icons'>
+                    <section className='pref-icon'>
+                        <GrUserSettings />
+                    </section>  
                     <a href='/preferences'><li>Preferences</li></a>
+                 </section>
                 </article>
             </section>
         </main>
