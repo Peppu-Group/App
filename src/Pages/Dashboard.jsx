67d12e087d057ae7439bb7efb8147615e42de808
choco-develop <onoigboriaose@gmail.com>
2023-01-10 19:17:32 +0100

    dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import Sidebar from '../Components/Sidebar'
-import IMG_2437 from '../assets/IMG_2437.png'
-
+import { FaSearch } from 'react-icons/fa'
 
 const Dashboard = () => {
   return (
+    <body id='dash-board'>
+      
+    
     <main className='dashboard-main'>
-      <nav>
+      <aside>
         <Sidebar />
-      </nav>
+      </aside>
+
+    <main>
+      <header className='fixed'> 
+      <h2>My&nbsp;Workspace</h2>
+      <nav className='search'><FaSearch/></nav>
+      </header>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste in ex dolor, soluta eos modi! Temporibus enim velit officiis! Quam autem odit quas harum vero fugiat tempora explicabo necessitatibus?</p>
+
     </main>
+    
+    </main>
+    </body>
   )
 }
 
