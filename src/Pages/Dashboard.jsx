91ec8a1c317b04677295edf6d6d069eec75af778
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-01-10 15:20:21 +0100

    made some changes

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Sidebar from '../Components/Sidebar'
+import IMG_2437 from '../assets/IMG_2437.png'
+
 
 const Dashboard = () => {
   return (
     <main className='dashboard-main'>
-    <header>
       <nav>
         <Sidebar />
       </nav>
-    </header>
     </main>
   )
 }
