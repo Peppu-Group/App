bc6024ecf6148649be7b29010a2f3d71f96efe42
choco-develop <onoigboriaose@gmail.com>
2023-01-12 20:30:41 +0100

    height of sidenav

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,23 +3,26 @@ import Sidebar from '../Components/Sidebar'
 import { FaSearch } from 'react-icons/fa'
 
 const Dashboard = () => {
+  
   return (
+   
+
     <body id='dash-board'>
-      
-    
     <main className='dashboard-main'>
-      <aside>
-        <Sidebar />
+  
+      <aside  className='aside'>
+        <Sidebar/>
       </aside>
 
-    <main>
-      <header className='fixed'> 
+    <section>
+      <header className='fixed' > 
       <h2>My&nbsp;Workspace</h2>
       <nav className='search'><FaSearch/></nav>
       </header>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste in ex dolor, soluta eos modi! Temporibus enim velit officiis! Quam autem odit quas harum vero fugiat tempora explicabo necessitatibus?</p>
-
-    </main>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iste in ex dolor, 
+        soluta eos modi! Temporibus enim velit officiis! Quam autem odit quas harum vero fugiat 
+        tempora explicabo necessitatibus?</p>
+    </section>
     
     </main>
     </body>
