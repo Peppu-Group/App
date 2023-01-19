539672373100a4dfd8d9ee5a1c09af92b8cc03ee
choco-develop <onoigboriaose@gmail.com>
2023-01-19 15:05:38 +0100

    login started

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const Login = () =>{
+    return (
+        <body className="login">
+            
+        
+<header>
+    <img></img>
+    <span>Welcome back!</span>
+</header>
+</body>
+    )
+}
