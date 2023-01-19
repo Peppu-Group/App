00cd3f8348e789cea10fdfa9a59f454d90268bbf
choco-develop <onoigboriaose@gmail.com>
2023-01-19 16:50:55 +0100

    sign-in page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Dashboard from './Pages/Dashboard'
 import Getstarted from './Pages/Getstarted'
+import Signin from './Pages/Signin'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Dashboard/>} />
           <Route exact path="/login" element={<Getstarted/>} />
+          <Route  exact path="/signin" element={<Signin/>} />
       </Routes>
       </Router>
     </div>
